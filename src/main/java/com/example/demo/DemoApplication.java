ea523129d627cea4d15e1fc41a2715f6914e11d7
juan.salcedo <jsalcedo@proteccion.com.co>
2022-01-31 16:29:02 -0500

    Primer manejo de errores y sobrecarga de metos en el suscriiber

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,21 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("juan","jose","javier","leo")
-				.doOnNext(System.out::println);
-		nombres.subscribe();
+		Flux<String> nombres = Flux.just("juan","jose","javier","","leo")
+				.doOnNext(e->{
+					if (e.isEmpty()){
+						throw new RuntimeException("El nombre no puede ser vacio");
+					}
+					System.out.println(e);
+				});
+		nombres.subscribe(e->log.info(e),error->log.error(error.getMessage()));
 	}
 }
